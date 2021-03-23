1695d8d95f1084a77d0ca86325379552f57e5a09
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-03-22 20:48:34 -0500

    WIP

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,8 +1,20 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { EventStoreDBClient, STREAM_NAME, streamNameFilter } from '@eventstore/db-client';
 
 async function bootstrap() {
+  const client = EventStoreDBClient.connectionString('esdb://localhost:2113?tls=false');
+  const prefixes = ["project-aggregate-"];
+  const filter = streamNameFilter({ prefixes });
+  const subscription = client
+    .subscribeToAll({ filter })
+    .on("data", function (resolvedEvent) {
+      console.log(
+        `Received event ${resolvedEvent.event.revision}@${resolvedEvent.event.streamId}`
+      );
+    });
+
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
