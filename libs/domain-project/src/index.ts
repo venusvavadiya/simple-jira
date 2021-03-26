ba19495a93206a8e93a2983dc376f108a6c810a8
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-03-25 19:49:40 -0500

    Command and CommandHandlers Scaffolded

--- libs/domain-project/src/index.ts
+++ libs/domain-project/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/domain-project';
+export * from './command/create-project/command-handler';
+export * from './command/create-project/command';
+export * from './command/rename-project/command-handler';
+export * from './command/rename-project/command';
