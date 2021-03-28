3e5b97c7b480d79be504040852468372d325846a
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-03-28 00:12:44 -0500

    Get Data

--- apps/web/src/app/app.controller.ts
+++ apps/web/src/app/app.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
-import { EventStoreDBClient } from '@eventstore/db-client';
-import { EventStoreDBEventStore } from '@simple-jira/adapter-event-store-db';
 import {
   CreateProjectCommandHandler,
   CreateProjectCommand,
-  ProjectAggregateRepository,
   RenameProjectCommandHandler,
   RenameProjectCommand,
 } from '@simple-jira/domain-project';
@@ -12,18 +9,10 @@ import { v4 as uuidv4 } from 'uuid';
 
 @Controller()
 export class AppController {
-  private readonly createProjectCommandHandler: CreateProjectCommandHandler
-  private readonly renameProjectCommandHandler: RenameProjectCommandHandler
-
-  constructor() {
-    const client = EventStoreDBClient.connectionString('esdb://localhost:2113?tls=false');
-    const eventStore = new EventStoreDBEventStore(client);
-
-    const projectAggRepo = new ProjectAggregateRepository(eventStore);
-
-    this.createProjectCommandHandler = new CreateProjectCommandHandler(projectAggRepo);
-    this.renameProjectCommandHandler = new RenameProjectCommandHandler(projectAggRepo);
-  }
+  constructor(
+    private readonly createProjectCommandHandler: CreateProjectCommandHandler,
+    private readonly renameProjectCommandHandler: RenameProjectCommandHandler,
+  ) {}
 
   @Get()
   async getData() {
--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
+import { EventStoreDBClient } from '@eventstore/db-client';
+import { EventStoreDBEventStore } from '@simple-jira/adapter-event-store-db';
+import {
+  CreateProjectCommandHandler,
+  ProjectAggregateRepository,
+  RenameProjectCommandHandler,
+} from '@simple-jira/domain-project';
 import { AppController } from './app.controller';
+
+function getProviderConfig<T>(instance: T): { provide: string, useValue: T } {
+  const provide = instance.constructor.name;
+  const useValue = instance;
+  return { provide, useValue };
+}
+
+const client = EventStoreDBClient.connectionString('esdb://localhost:2113?tls=false');
+const eventStore = new EventStoreDBEventStore(client);
+
+const projectAggRepo = new ProjectAggregateRepository(eventStore);
 
 @Module({
   imports: [],
   controllers: [AppController],
+  providers: [
+    getProviderConfig(new CreateProjectCommandHandler(projectAggRepo)),
+    getProviderConfig(new RenameProjectCommandHandler(projectAggRepo)),
+  ],
 })
 export class AppModule {}
