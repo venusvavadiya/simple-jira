00ef0eadde3f48093c22210b7bcb8ddf9288c3e1
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-03-28 21:13:36 -0500

    Event store db (#14)
    * New Lib created
    
    * Adapter written
    
    * Code cleaned
    
    * Integration done
    
    * EventTypes improved
    
    * Fixed
    
    * Get Data
    
    * Providers created in AppModule

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,8 +1,34 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { EventStoreDBClient } from '@eventstore/db-client';
+import { EventStoreDBEventStore } from '@simple-jira/adapter-event-store-db';
+import {
+  CreateProjectCommandHandler,
+  ProjectAggregateRepository,
+  RenameProjectCommandHandler,
+} from '@simple-jira/domain-project';
 import { AppController } from './app.controller';
 
+function getProviderConfig<T>(instance: T): { provide: string, useValue: T } {
+  const provide = instance.constructor.name;
+  const useValue = instance;
+  return { provide, useValue };
+}
+
 @Module({
-  imports: [],
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule {
+  static register(): DynamicModule {
+    const client = EventStoreDBClient.connectionString('esdb://localhost:2113?tls=false');
+    const eventStore = new EventStoreDBEventStore(client);
+
+    const projectAggRepo = new ProjectAggregateRepository(eventStore);
+
+    const providers = [
+      getProviderConfig(new CreateProjectCommandHandler(projectAggRepo)),
+      getProviderConfig(new RenameProjectCommandHandler(projectAggRepo)),
+    ];
+
+    return { module: AppModule, providers };
+  }
+}
