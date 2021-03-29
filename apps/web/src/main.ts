cb93f3bcef127103778e55d126810731d6fcbdf0
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-03-28 21:04:58 -0500

    Instanciated

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,23 +1,21 @@
 import { EventStoreDBClient } from '@eventstore/db-client';
 import { NestFactory } from '@nestjs/core';
-import { EventStoreDBEventSubscription } from '@simple-jira/adapter-event-store-db';
+import { EventStoreDBEventStore, EventStoreDBEventSubscription } from '@simple-jira/adapter-event-store-db';
 import { MongoClient } from 'mongodb';
 import { AppModule } from './app/app.module';
 import { environment } from './environment';
-import { ProjectAggregateMongoDBEventListener } from './app/project-aggregate-mongo-db.event-listener';
 
 async function bootstrap() {
-  const client = EventStoreDBClient.connectionString('esdb://localhost:2113?tls=false');
-  const subscription = new EventStoreDBEventSubscription(client);
-
   const mongoClient = new MongoClient('mongodb://root:root@localhost:27017', { useNewUrlParser: true });
   await mongoClient.connect();
-  const database = mongoClient.db('simple-jira');
-  const projectsCollection = database.collection('projects');
 
-  subscription.register(new ProjectAggregateMongoDBEventListener(projectsCollection));
+  const eventStoreDBClient = EventStoreDBClient.connectionString('esdb://localhost:2113?tls=false');
 
-  const app = await NestFactory.create(AppModule.register());
+  const app = await NestFactory.create(AppModule.register(
+    new EventStoreDBEventStore(eventStoreDBClient),
+    new EventStoreDBEventSubscription(eventStoreDBClient),
+    mongoClient.db('simple-jira'),
+  ));
   const { port } = environment;
   await app.listen(port);
 }
