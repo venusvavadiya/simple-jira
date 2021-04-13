256ac67dd23ab90a4d1de10cf25fe2ff30859ce3
Shailendra Shukla <shailendrashuklapro@gmail.com>
2021-04-12 22:25:10 -0500

    Remove Query Imports
    Co-authored-by: Venus Vavadiya <thisisvenusvavadiya@gmail.com>

--- apps/web/src/app/projects.resolver.ts
+++ apps/web/src/app/projects.resolver.ts
@@ -1,6 +1,4 @@
-import {
-  Args, Mutation, Query, Resolver,
-} from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import {
   CreateProjectCommand,
   CreateProjectCommandHandler,
