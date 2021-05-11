592c239010a39d0e27c9ed6659202ba71b665497
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-05-11 17:26:19 -0500

    Plugin removed

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,12 +1,13 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 import App from './App.vue';
 import router from './router';
-import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
 new Vue({
   router,
-  vuetify,
+  vuetify: new Vuetify(),
   render: (h) => h(App),
 }).$mount('#app');
