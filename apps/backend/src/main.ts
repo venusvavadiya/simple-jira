14823b31da47ebc8dc8d1213368ec7e01a4c2c87
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-05-11 17:31:02 -0500

    Client cleaned

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,16 +10,19 @@ const environment = {
   port: 8000,
 };
 
-async function bootstrap() {
-  const { eventStoreDBUrl, mongoDBUrl, port } = environment;
-
+async function getMongoDBClient(url: string): Promise<MongoClient> {
   const useNewUrlParser = true;
   const useUnifiedTopology = true;
-  const mongoDBConfig = { useNewUrlParser, useUnifiedTopology };
-  const mongoDBClient = new MongoClient(mongoDBUrl, mongoDBConfig);
-  await mongoDBClient.connect();
+  const config = { useNewUrlParser, useUnifiedTopology };
+  const client = new MongoClient(url, config);
+  await client.connect();
+  return client;
+}
 
+async function bootstrap() {
+  const { eventStoreDBUrl, mongoDBUrl, port } = environment;
   const eventStoreDBClient = EventStoreDBClient.connectionString(eventStoreDBUrl);
+  const mongoDBClient = await getMongoDBClient(mongoDBUrl);
 
   const app = await NestFactory.create(AppModule.register(
     new EventStoreDBEventStore(eventStoreDBClient),
