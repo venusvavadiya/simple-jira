39be58ad867cbaf0c2f3576085d0f181134383d6
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-05-13 17:58:29 -0500

    Removed

--- apps/backend/src/app/repositories/project-repository.ts
+++
@@ -1,6 +0,0 @@
-import { Project } from '../entities/project';
-
-export declare interface ProjectRepository {
-  existsById(projectId: string): Promise<boolean>;
-  save(project: Project): Promise<void>;
-}
