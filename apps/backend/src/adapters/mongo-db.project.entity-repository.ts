e5b4d677431fa463bdbb5e9ad7c904eab60fa04a
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-05-14 19:08:28 -0500

    Abstracted to base repository

--- apps/backend/src/adapters/mongo-db.project.entity-repository.ts
+++ apps/backend/src/adapters/mongo-db.project.entity-repository.ts
@@ -1,5 +1,6 @@
 import { ProjectEntity, ProjectEntityRepository } from '@simple-jira/domain-project';
 import { MongoDBRepository } from './mongodb.repository';
 
-export class MongoDBProjectEntityRepository extends MongoDBRepository<ProjectEntity>
+export class MongoDBProjectEntityRepository
+  extends MongoDBRepository<ProjectEntity>
   implements ProjectEntityRepository {}
